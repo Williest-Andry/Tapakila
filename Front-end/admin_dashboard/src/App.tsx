24cf472ba15556d8a3673bc1b745ab52cdaa6289
nonieKley <hei.oninaina@gmail.com>
2025-03-15 11:18:44 +0300

    finalize design

--- Front-end/admin_dashboard/src/App.tsx
+++ Front-end/admin_dashboard/src/App.tsx
@@ -1,14 +1,31 @@
-import { Admin, Resource } from 'react-admin';
+// App.tsx
+import { Admin, Resource, radiantLightTheme, radiantDarkTheme } from 'react-admin';
 import { EventList } from './Events/EventLists';
 import { EventCreate } from './Events/EventCreate';
 import { EventEdit } from './Events/EventUpdate';
 import { dataProvider } from './Events/DataProvider';
+import { Layout } from 'react-admin';
+import { ReactNode } from 'react';
+import './App.css';
 
-
+const CustomLayout = (props: { children: ReactNode }) => (
+  <Layout {...props} className="custom-layout" />
+);
 
 const App = () => (
-  <Admin dataProvider={dataProvider}>
-    <Resource name="events" list={EventList} create={EventCreate} edit={EventEdit} />
+  <Admin 
+    dataProvider={dataProvider} 
+    layout={CustomLayout}
+    theme={radiantLightTheme}
+    darkTheme={radiantDarkTheme}
+  >
+    <Resource
+      name="events"
+      list={EventList}
+      create={EventCreate}
+      edit={EventEdit}
+      options={{ label: "Events" }}
+    />
   </Admin>
 );
 
