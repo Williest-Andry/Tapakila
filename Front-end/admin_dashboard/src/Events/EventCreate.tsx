71037d00a0462a6d4e1bfc97984b877e5cb2b115
nonieKley <hei.oninaina@gmail.com>
2025-04-02 17:03:24 +0300

    change interface for eventCreate.tsx , removed the old way

--- Front-end/admin_dashboard/src/Events/EventCreate.tsx
+++ Front-end/admin_dashboard/src/Events/EventCreate.tsx
@@ -1,15 +1,27 @@
-import { Create, SimpleForm, TextInput, DateInput, SelectInput } from 'react-admin';
+import { Create, SimpleForm, TextInput, DateInput, SelectInput, NumberInput, FileInput, FileField } from 'react-admin';
 
 export const EventCreate = () => (
   <Create>
     <SimpleForm>
       <TextInput source="title" label="Titre" required />
-      <DateInput source="date" label="Date" required />
-      <SelectInput source="status" label="Statut" choices={[
-        { id: 'draft', name: 'Brouillon' },
-        { id: 'published', name: 'Publié' },
-        { id: 'canceled', name: 'Annulé' },
-      ]} required />
+      <TextInput source="description" label="Description" multiline required />
+      <DateInput source="dateTime" label="Date et Heure" required />
+      <FileInput source="image" label="Image" accept="image/*" required>
+        <FileField source="src" title="title" />
+      </FileInput>
+      <TextInput source="location" label="Lieu" required />
+      <TextInput source="category" label="Catégorie" required />
+      <NumberInput source="availablePlace" label="Places disponibles" required min={0} />
+      <SelectInput
+        source="status"
+        label="Statut"
+        choices={[
+          { id: 'draft', name: 'Brouillon' },
+          { id: 'published', name: 'Publié' },
+          { id: 'canceled', name: 'Annulé' },
+        ]}
+        required
+      />
     </SimpleForm>
   </Create>
 );
