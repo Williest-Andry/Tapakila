42017fd63a4266fd2eb35776e550ea916040a7e6
nonieKley <hei.oninaina@gmail.com>
2025-04-02 17:09:48 +0300

    feat : change eventUpdate form

--- Front-end/admin_dashboard/src/Events/EventUpdate.tsx
+++ Front-end/admin_dashboard/src/Events/EventUpdate.tsx
@@ -1,15 +1,24 @@
-import { Edit, SimpleForm, TextInput, DateInput, SelectInput } from 'react-admin';
+import { Edit, SimpleForm, TextInput, DateInput, SelectInput, NumberInput } from 'react-admin';
 
 export const EventEdit = () => (
   <Edit>
     <SimpleForm>
       <TextInput source="title" label="Titre" required />
-      <DateInput source="date" label="Date" required />
-      <SelectInput source="status" label="Statut" choices={[
-        { id: 'draft', name: 'Brouillon' },
-        { id: 'published', name: 'Publié' },
-        { id: 'canceled', name: 'Annulé' },
-      ]} required />
+      <TextInput source="description" label="Description" multiline required />
+      <DateInput source="dateTime" label="Date et Heure" required />
+      <TextInput source="location" label="Lieu" required />
+      <TextInput source="category" label="Catégorie" required />
+      <NumberInput source="availablePlace" label="Places disponibles" required min={0} />
+      <SelectInput
+        source="status"
+        label="Statut"
+        choices={[
+          { id: 'draft', name: 'Brouillon' },
+          { id: 'published', name: 'Publié' },
+          { id: 'canceled', name: 'Annulé' },
+        ]}
+        required
+      />
     </SimpleForm>
   </Edit>
 );
