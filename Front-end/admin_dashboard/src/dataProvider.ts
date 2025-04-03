90ebf571020966721a95d7e1b7390d4c8441cc1e
Kef <hei.kephas@gmail.com>
2025-04-03 19:38:05 +0300

    nilalizing

--- Front-end/admin_dashboard/src/dataProvider.ts
+++ Front-end/admin_dashboard/src/dataProvider.ts
@@ -1,20 +1,30 @@
 import { DataProvider, fetchUtils } from "react-admin";
 
 const apiUrl = "http://localhost:3001";
-const httpClient = fetchUtils.fetchJson;
+const httpClient = (url: string, options = {}) => {
+  return fetchUtils.fetchJson(url, options);
+};
 
 
 const dataProvider: DataProvider = {
-  getList: async (resource) => {
+  getList: async (resource, params) => {
+
+    const { page, perPage } = params.pagination ?? { page: 1, perPage: 25 };
+    const { field, order } = params.sort ?? { field: "id", order: "ASC" };
+
     const options: fetchUtils.Options = {
       headers: new Headers({
         Accept: "application/json",
         Authorization: `Bearer ${localStorage.getItem("authToken")? localStorage.getItem("authToken") : ""}`,
       }),
     };
-    const url = `${apiUrl}/${resource}`;
+
+    const url = `${apiUrl}/${resource}?filter=%7B%7D&order=${order}&page=${page}&perPage=${perPage}&sort=${field}`;
+    
     const { json } = await httpClient(url, options);
-    return { data: json, total: json.length };
+    const obj = (await fetchUtils.fetchJson(`${apiUrl}/${resource}`)).json
+
+    return { data: json, total: obj.length };
   },
 
   getOne: async (resource, params) => {
